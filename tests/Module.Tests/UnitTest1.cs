739d28edd1648551878d5e248d480b5704fbe7f9
shblkjo <daria_afanaseva2005@mail.ru>
2026-05-29 00:17:40 +0300

    Update UnitTest1.cs

--- tests/Module.Tests/UnitTest1.cs
+++ tests/Module.Tests/UnitTest1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace Lab.Tests;
 
-[TestFixture(typeof(Lab.Implementations.GenCode1.TemperatureConverter), Category = "GenCode1")]
+//[TestFixture(typeof(Lab.Implementations.GenCode1.TemperatureConverter), Category = "GenCode1")]
 [TestFixture(typeof(Lab.Implementations.GenCode2.TemperatureConverter), Category = "GenCode2")]
-[TestFixture(typeof(Lab.Implementations.GenCode3.TemperatureConverter), Category = "GenCode3")]
+//[TestFixture(typeof(Lab.Implementations.GenCode3.TemperatureConverter), Category = "GenCode3")]
 
 public class TemperatureConverterTests
 {
